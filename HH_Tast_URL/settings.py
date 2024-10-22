25c90f465b7791d71822a588baf28d44e846faac
Bilal Ahmed <47696353+bilalahmedkhatri@users.noreply.github.com>
2024-10-22 19:33:59 +0000

    from codespaces

--- HH_Tast_URL/settings.py
+++ HH_Tast_URL/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- shortener/forms.py
+++ shortener/forms.py
@@ -4,4 +4,16 @@ from .models import ShortenedURL
 class ShortenURLForm(forms.ModelForm):
     class Meta:
         model = ShortenedURL
-        fields = ['original_url']
+        fields = '__all__'
+        
+        label = {
+            'url': 'original_url',
+            'short_code': 'Short Code',
+        }
+        
+        widget = {
+            'url' : forms.TextInput(attrs={'placeholder': 'URL'}),
+            'short_code' : forms.TextInput(attrs={'readonly': True}),
+            
+        }
+        
